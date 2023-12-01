a50c03dd6c413a5ee62d7d316409239a1e44c1bc
Seppe Stroobants <seppestroobants@Seppes-MBP.lan>
2023-12-01 15:42:49 +0100

    login almost done

---
+++ code/monitoringapplicatie_web_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:monitoringapplicatie_web_app/pages/login_web.dart';
+import 'package:monitoringapplicatie_web_app/pages/web_dashboard_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyBEWWEmntLEDFqy4taCyObSj-fiFQuzX74",
+    projectId: "mobi-12",
+    messagingSenderId: "281097569077",
+    appId: "1:281097569077:web:e15118a28e149aa03cc27c",
+  ));
+  runApp(MaterialApp(
+    initialRoute: '/login_web',
+    routes: {
+      '/web_dashboard_page': (context) => WebPage(),
+      '/login_web': (context) => const LoginWeb(),
+    },
+  ));
+}
